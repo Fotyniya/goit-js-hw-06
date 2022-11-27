a18c735fa22d93aa92e587b602bace7f0b0436d8
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-11-27 09:24:15 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formElements = {
+        email,
+        password
+    };
+    console.table(formElements)
+
+    const formData = new FormData (event.currentTarget);
+    
+    formData.forEach ((value, name) =>
+       (!value) ? alert('Всі поля повинні бути заповнені'): 
+       event.currentTarget.reset() 
+    )
+};
