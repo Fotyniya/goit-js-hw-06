b5e930016f87177127580868800d107f41dcadc5
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-11-27 13:51:11 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+const body = document.querySelector('body');
+const nameColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', onBtnChangeColorClick);
+function onBtnChangeColorClick () {
+
+const color = getRandomHexColor();
+body.style.backgroundColor = color;
+nameColor.textContent = color;
 }
